688bc796e3f1df413fc4a1e858846243e2a22569
Dmitrii Kocheshkov <dimakid123@gmail.com>
2018-11-30 19:59:29 +0300

    first ver of file saving added

--- app/src/main/java/i/maxmol/hackapp/MainActivity.java
+++ app/src/main/java/i/maxmol/hackapp/MainActivity.java
@@ -2,6 +2,7 @@ package i.maxmol.hackapp;
 
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.pm.ActivityInfo;
 import android.graphics.Camera;
 import android.os.Bundle;
@@ -10,13 +11,17 @@ import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 import io.fotoapparat.Fotoapparat;
 import io.fotoapparat.log.LoggersKt;
 import io.fotoapparat.parameter.ScaleType;
 import io.fotoapparat.preview.FrameProcessor;
+import io.fotoapparat.result.BitmapPhoto;
+import io.fotoapparat.result.PhotoResult;
 import io.fotoapparat.selector.FlashSelectorsKt;
 import io.fotoapparat.selector.FocusModeSelectorsKt;
 import io.fotoapparat.selector.LensPositionSelectorsKt;
@@ -51,6 +56,14 @@ public class MainActivity extends Activity {
                 .build();
         fotoapparat.start();
         fotoapparat.takePicture();
+        PhotoResult photoResult = fotoapparat.takePicture();   // Asynchronously saves photo to file
+        try {
+            File file = File.createTempFile("img","jpg",context.getFilesDir());
+            photoResult.saveToFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
         try {
             File file = new File(context.getFilesDir() + "/db.txt");
             Scanner scanner = new Scanner(file);
