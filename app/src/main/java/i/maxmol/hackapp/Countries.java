74f5f49b54993e525cd8df2ce745e34c0f9fc598
maxmol <maxmolyt@gmail.com>
2018-11-30 16:45:18 +0300

    asd

---
+++ app/src/main/java/i/maxmol/hackapp/Countries.java
@@ -0,0 +1,13 @@
+package i.maxmol.hackapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Countries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countries);
+    }
+}
